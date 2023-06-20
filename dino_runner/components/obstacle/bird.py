49e324828c774d49c7726436ffb4cd15f10dcd7c
JeanVictor <106544372+JeanVictor81@users.noreply.github.com>
2023-06-20 15:05:28 -0300

    Bird update

--- dino_runner/components/obstacle/bird.py
+++ dino_runner/components/obstacle/bird.py
@@ -1,13 +1,23 @@
 import random
 
 from dino_runner.components.obstacle.obstacle import Obstacle
+from dino_runner.utils.constants import BIRD
 
 class Bird(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 1)
+        self.step_index = 0
         super().__init__(image, self.type)
 
         if self.type == 0:
             self.rect.y = 260
         elif self.type == 1:
             self.rect.y = 300
+    
+    def draw(self, screen):
+        if self.step_index >= 10:
+            self.step_index = 0
+        screen.blit(self.image[self.step_index//5],(self.rect.x, self.rect.y))
+        self.step_index += 1
+        
+
